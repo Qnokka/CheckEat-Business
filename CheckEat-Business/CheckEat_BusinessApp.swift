e96583c8c4eda12ca87d33922d46c47e76aa63d4
Qnokka <wnsdud010911@outlook.kr>
2025-07-04 16:14:09 +0900

    initial settings

---
+++ CheckEat-Business/CheckEat_BusinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckEat_BusinessApp.swift
+//  CheckEat-Business
+//
+//  Created by 최준영 on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CheckEat_BusinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
