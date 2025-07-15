eac434d90b1d4497bff2b955d35c7e894355bdf0
Qnokka <wnsdud010911@outlook.kr>
2025-07-16 02:44:06 +0900

    Feat: 스타일 코드 추가 및 스타일 파일명 수정
    - UnderLine 적용된 텍스트필드 및 텍스트

--- CheckEat-Business/Views/Auth/Style/UnderLinedStyle.swift
+++ CheckEat-Business/Views/Auth/Style/UnderLinedStyle.swift
@@ -32,3 +32,21 @@ struct UnderLinedTextField: View {
         }
     }
 }
+
+struct UnderLinedText: View {
+    @Binding var text: String
+    @FocusState private var isFocused: Bool
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4){
+            Text(text)
+                .multilineTextAlignment(.leading)
+                .padding(.vertical, 8)
+                .focused($isFocused)
+            Rectangle()
+                .frame(height: 1)
+                .foregroundColor(isFocused || !text.isEmpty ? .black : Color(red: 0.85, green: 0.85, blue: 0.85))
+                .animation(.easeInOut(duration: 0.1), value: isFocused)
+                .padding(.bottom)
+        }
+    }
+}
