04aa251c3cfb9d82a978f834661d28eb74651855
Qnokka <wnsdud010911@outlook.kr>
2025-07-31 10:11:28 +0900

    Feat: 홈 뷰 화면 연결

--- CheckEat-Business/CheckEat_BusinessApp.swift
+++ CheckEat-Business/CheckEat_BusinessApp.swift
@@ -35,3 +35,53 @@ struct CheckEat_BusinessApp: App {
         }
     }
 }
+
+struct CheckEat_BusinessApp1: App {
+    
+    @State private var selectedTab: CustomTabBarView.Tab = .home
+    //TODO: 언어 설정 값 가져오는 로직 구현
+    
+    var body: some Scene {
+        WindowGroup {
+            
+            VStack {
+                ZStack {
+                    switch selectedTab {
+                    case .home:
+                        HomeMainView()
+                    case .menu:
+                        AddMenuView()
+                    case .myPage:
+                        MyPageView()
+                    }
+                }
+                .frame(maxHeight: .infinity)
+                
+                CustomTabBarView(selectedTab: $selectedTab)
+            }
+            .background(Color.white)
+            
+            //TODO: 추후 다음과 같이 수정
+//            if signmanager.login { //로그인된 상태가 아니라면 홈으로 진입
+//                VStack {
+//                    ZStack {
+//                        switch selectedTab {
+//                        case .home:
+//                            HomeMainView()
+//                        case .menu:
+//                            AddMenuView()
+//                        case .myPage:
+//                            MyPageView()
+//                        }
+//                    }
+//                    .frame(maxHeight: .infinity)
+//                    
+//                    CustomTabBarView(selectedTab: $selectedTab)
+//                }
+//                .background(Color.white)
+//            } else {
+//                LoginView()
+//            }
+        }
+    }
+}
